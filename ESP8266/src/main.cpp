3d1d28ba3562356c3630b9277cf824809a832412
Nguyễn Danh Nam <izero.cs@gmail.com>
2020-04-25 23:14:08 +0700

    Interval upload

---
+++ ESP8266/src/main.cpp
@@ -0,0 +1,38 @@
+#include <Arduino.h>
+
+#define PIN_COUNT_MAP 3
+
+unsigned long timerOne = 0;
+
+uint8_t pinInMaps[PIN_COUNT_MAP]   = { 14, 12, 13 };
+uint8_t pinOutMaps[PIN_COUNT_MAP]  = { 16,  5,  4 };
+uint8_t pinInStatus[PIN_COUNT_MAP] = {  0,  0,  0 };
+
+void setup() {
+    Serial.begin(115200);
+
+    for (int i = 0; i < PIN_COUNT_MAP; ++i) {
+        pinMode(pinInMaps[i], INPUT);
+        pinMode(pinOutMaps[i], OUTPUT);
+
+        digitalWrite(pinOutMaps[i], LOW);
+    }
+}
+
+void loop() {
+    if ((unsigned long)(millis() - timerOne) > 100) {
+        for (int i = 0; i < PIN_COUNT_MAP; ++i) {
+            if (digitalRead(pinInMaps[i]) == LOW) {
+                if (pinInStatus[i] == 0) {
+                    pinInStatus[i] = 1;
+                    digitalWrite(pinOutMaps[i], HIGH);
+                }
+            } else if (pinInStatus[i] == 1) {
+                pinInStatus[i] = 0;
+                digitalWrite(pinOutMaps[i], LOW);
+            }
+        }
+
+        timerOne = millis();
+    }
+}
--- Server/api/room/types.js
+++ Server/api/room/types.js
@@ -1,15 +1,5 @@
 module.exports = ({ route }) => {
     route.get((req, res) => {
-        res.json([
-            "living_room",
-            "bed_room",
-            "kitchen_room",
-            "bath_room",
-            "balcony_room",
-            "stairs_room",
-            "fence_room",
-            "mezzanine_room",
-            "roof_room"
-        ])
+        res.json()
     });
 };
