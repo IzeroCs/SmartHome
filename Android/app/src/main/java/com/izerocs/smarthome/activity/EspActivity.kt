dd1b035f9fd0523e047c8ff6eda9e9495a5c0db7
Nguyễn Danh Nam <izero.cs@gmail.com>
2020-05-15 00:46:49 +0700

    Interval upload

--- Android/app/src/main/java/com/izerocs/smarthome/activity/EspActivity.kt
+++ Android/app/src/main/java/com/izerocs/smarthome/activity/EspActivity.kt
@@ -2,58 +2,26 @@ package com.izerocs.smarthome.activity
 
 import android.animation.Animator
 import android.animation.ObjectAnimator
-import android.content.BroadcastReceiver
-import android.content.Context
-import android.content.Intent
-import android.content.IntentFilter
-import android.net.ConnectivityManager
-import android.net.ConnectivityManager.NetworkCallback
-import android.net.Network
-import android.net.NetworkCapabilities
-import android.net.NetworkRequest
-import android.net.wifi.WifiConfiguration
-import android.net.wifi.WifiInfo
 import android.net.wifi.WifiManager
-import android.net.wifi.WifiNetworkSpecifier
-import android.os.Build
 import android.os.Bundle
-import android.provider.Settings
 import android.view.View
-import androidx.annotation.RequiresApi
 import com.izerocs.smarthome.R
 import com.izerocs.smarthome.adapter.ListEspAdapter
 import com.izerocs.smarthome.model.EspItem
+import com.izerocs.smarthome.network.EspConnectivity
 import com.izerocs.smarthome.preferences.EspPreferences
 import com.izerocs.smarthome.preferences.SharedPreferences
-import com.izerocs.smarthome.utils.Util
 import es.dmoral.toasty.Toasty
 import kotlinx.android.synthetic.main.activity_esp.*
-import java.net.ConnectException
-import java.net.HttpURLConnection
-import java.net.URL
 
 /**
  * Created by IzeroCs on 2020-04-30
  */
-class EspActivity : BaseActivity(), View.OnClickListener, ListEspAdapter.OnItemClickListener {
-    private var isScan : Boolean = false
-
-    private var currentWifiInfo : WifiInfo? = null
-    private var refreshAnimator : ObjectAnimator? = null
-    private var wifiManager : WifiManager? = null
-    private var connectivityManager : ConnectivityManager? = null
-    private var networkCallback : NetworkCallback? = null
-
-    private val wifiScanReceiver = object : BroadcastReceiver() {
-        override fun onReceive(context : Context, intent : Intent) {
-            if (intent.getBooleanExtra(WifiManager.EXTRA_RESULTS_UPDATED, false))
-                this@EspActivity.scanSuccess()
-            else
-                this@EspActivity.scanFailed()
-
-            applicationContext.unregisterReceiver(this)
-        }
-    }
+class EspActivity : BaseActivity(), View.OnClickListener, ListEspAdapter.OnItemClickListener,
+    EspConnectivity.OnScanerListener
+{
+    private var espConnectivity : EspConnectivity? = null
+    private var refreshAnimator : ObjectAnimator?  = null
 
     private val onAnimatorListener = object : Animator.AnimatorListener {
         override fun onAnimationRepeat(animation : Animator?) { }
@@ -71,6 +39,7 @@ class EspActivity : BaseActivity(), View.OnClickListener, ListEspAdapter.OnItemC
 
     }
 
+
     override fun onCreate(savedInstanceState : Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_esp)
@@ -79,10 +48,7 @@ class EspActivity : BaseActivity(), View.OnClickListener, ListEspAdapter.OnItemC
         listEspScan.setOnItemClickListener(this)
         listEspConnected.setOnItemClickListener(this)
 
-        wifiManager = applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
-        connectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-        currentWifiInfo = wifiManager?.connectionInfo
-
+        espConnectivity = EspConnectivity(this)
         refreshAnimator = ObjectAnimator.ofFloat(floatButton, "rotation", 0F, 360F)
             .apply {
                 duration    = 800
@@ -104,15 +70,15 @@ class EspActivity : BaseActivity(), View.OnClickListener, ListEspAdapter.OnItemC
             }
         }
 
-        startScan()
+        espConnectivity?.run {
+            setOnScannerListener(this@EspActivity)
+            startScanModule()
+        }
     }
 
     override fun onDestroy() {
         super.onDestroy()
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-            networkCallback?.let { connectivityManager?.unregisterNetworkCallback(networkCallback) }
-        }
+        espConnectivity?.destroy()
     }
 
     override fun onCreatePreferences() : SharedPreferences? {
@@ -121,58 +87,28 @@ class EspActivity : BaseActivity(), View.OnClickListener, ListEspAdapter.OnItemC
 
     override fun onClick(v : View?) {
         if (v == floatButton) {
-            if (!isScan)
-                startScan()
+            espConnectivity?.toggleScanModule()
         }
     }
 
-    override fun onItemClick(v : View?, position : Int, isLongClick : Boolean) {
-        v?.parent.run {
-            if (this == listEspScan)
-                onItemScanClick(v as View, position)
-            else if (this == listEspConnected)
-                onItemConnectedClick(v as View, position)
+    override fun onScanBegin() {
+        runOnUiThread {
+            if (!refreshAnimator!!.isRunning) refreshAnimator?.run {
+                repeatCount = ObjectAnimator.INFINITE
+                start()
+            }
         }
     }
 
-    private fun onItemScanClick(v : View, position : Int) {
-        addWifiToModule(listEspScan.get(position))
-    }
-
-    private fun onItemConnectedClick(v : View, position : Int) {
-
-    }
-
-    private fun startScan() {
-        if (isScan)
-            return
-
-        applicationContext.registerReceiver(wifiScanReceiver, IntentFilter().apply {
-            addAction(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION)
-        })
-
-        if (!wifiManager!!.startScan())
-            scanFailed()
-
-        isScan = true
-
-        if (!refreshAnimator!!.isRunning) refreshAnimator?.run {
-            repeatCount = ObjectAnimator.INFINITE
-            start()
+    override fun onScanEnd() {
+        runOnUiThread {
+            if (refreshAnimator!!.isRunning)
+                refreshAnimator?.repeatCount = 1
         }
     }
 
-    private fun cancelScan() {
-        isScan = false
-
-        if (refreshAnimator!!.isRunning)
-            refreshAnimator?.repeatCount = 1
-    }
-
-    private fun scanSuccess() {
-        cancelScan()
-
-        wifiManager?.scanResults?.run {
+    override fun onScanSuccess(wifiManager : WifiManager) {
+        wifiManager.scanResults?.run {
             listEspScan.clear()
             preferences?.clear()
 
@@ -196,126 +132,26 @@ class EspActivity : BaseActivity(), View.OnClickListener, ListEspAdapter.OnItemC
         }
     }
 
-    private fun scanFailed() {
+    override fun onScanFailed(wifiManager : WifiManager) {
         runOnUiThread {
-            cancelScan()
             Toasty.error(this, R.string.listEspScanFailed, Toasty.LENGTH_LONG).show()
         }
     }
 
-    private fun isScanThrottleEnabled() : Boolean {
-        return Settings.Global.getInt(contentResolver, "wifi_scan_throttle_enabled") == 1
-    }
-
-    private fun addWifiToModule(item : EspItem) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q)
-            connectNetworkAndroidQ(item)
-        else
-            connectNetworkPreAndroidQ(item)
-    }
-
-    @Suppress("DEPRECATION")
-    private fun connectNetworkPreAndroidQ(item : EspItem) {
-        val wifi = WifiConfiguration().apply {
-            this.SSID = "\"${item.getSsid()}\""
-            this.preSharedKey = "\"${item.getSc()}\""
-            this.status = WifiConfiguration.Status.ENABLED
-
-            this.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.TKIP)
-            this.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.CCMP)
-            this.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.WPA_PSK)
-            this.allowedPairwiseCiphers.set(WifiConfiguration.PairwiseCipher.TKIP)
-            this.allowedPairwiseCiphers.set(WifiConfiguration.PairwiseCipher.CCMP)
-            this.allowedProtocols.set(WifiConfiguration.Protocol.RSN)
-        }
-
-        wifiManager?.run {
-            val id = this.addNetwork(wifi)
-
-            println("addNetwork: $id")
-
-            if (id != -1) {
-                disconnect()
-                enableNetwork(id, false)
-                reconnect()
-                sendWifiToModule()
-            } else {
-                failedConnectNetwork()
-            }
+    override fun onItemClick(v : View?, position : Int, isLongClick : Boolean) {
+        v?.parent.run {
+            if (this == listEspScan)
+                onItemScanClick(v as View, position)
+            else if (this == listEspConnected)
+                onItemConnectedClick(v as View, position)
         }
     }
 
-    @RequiresApi(api = Build.VERSION_CODES.Q)
-    private fun connectNetworkAndroidQ(item : EspItem) : Boolean {
-        val specifier = WifiNetworkSpecifier.Builder()
-            .setSsid(item.getSsid())
-            .setWpa2Passphrase(item.getSc())
-            .build()
-
-        val request = NetworkRequest.Builder()
-            .addTransportType(NetworkCapabilities.TRANSPORT_WIFI)
-            .removeCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
-            .setNetworkSpecifier(specifier)
-            .build()
-
-        networkCallback = object : ConnectivityManager.NetworkCallback() {
-            override fun onAvailable(network : Network) {
-                super.onAvailable(network)
-                sendWifiToModule()
-            }
-
-            override fun onUnavailable() {
-                super.onUnavailable()
-                connectivityManager?.unregisterNetworkCallback(networkCallback)
-                failedConnectNetwork()
-            }
-        }
-
-        connectivityManager?.requestNetwork(request, networkCallback)
-        return false
+    private fun onItemScanClick(v : View, position : Int) {
+        espConnectivity?.addWifiToModule(listEspScan.get(position))
     }
 
-    private fun sendWifiToModule() {
-        wifiManager?.run {
-            val ipAddress = Util.formatIPAddress(dhcpInfo.gateway)
-
-            println("sendWifiToModule: $ipAddress")
-
-            if (ipAddress != "192.168.31.15")
-                return failedValidateNetwork()
-
-            println("Send wifi to module: $ipAddress")
-
-            val url = URL("http://${ipAddress}/wifi")
-            val builder = StringBuilder()
-
-            try {
-                with(url.openConnection() as HttpURLConnection) {
-                    requestMethod = "GET"
-
-                    println("\nSent 'GET' request to URL : $url; Response Code : $responseCode")
-
-                    inputStream.bufferedReader(Charsets.UTF_8).use {
-                        it.forEachLine { builder.append(it) }
-
-                        println("Builder: ${builder.toString()}")
-                    }
-                }
-            } catch (e : ConnectException) {
-                e.printStackTrace()
-            }
-        }
-    }
-
-    private fun failedConnectNetwork() {
-
-    }
-
-    private fun failedValidateNetwork() {
-
-    }
-
-    private fun failedSendDataNetwork() {
+    private fun onItemConnectedClick(v : View, position : Int) {
 
     }
 }
---
+++ Android/app/src/main/java/com/izerocs/smarthome/network/EspConnectivity.kt
@@ -0,0 +1,267 @@
+package com.izerocs.smarthome.network
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.net.*
+import android.net.ConnectivityManager.NetworkCallback
+import android.net.wifi.WifiConfiguration
+import android.net.wifi.WifiInfo
+import android.net.wifi.WifiManager
+import android.net.wifi.WifiNetworkSpecifier
+import android.os.Build
+import android.provider.Settings
+import androidx.annotation.RequiresApi
+import com.izerocs.smarthome.model.EspItem
+import com.izerocs.smarthome.utils.Util
+import java.net.ConnectException
+import java.net.HttpURLConnection
+import java.net.URL
+
+/**
+ * Created by IzeroCs on 2020-05-14
+ */
+class EspConnectivity(private val context : Context) {
+    private val wifiManager = context.applicationContext
+        .getSystemService(Context.WIFI_SERVICE) as WifiManager
+
+    private val connectivityManager = context.applicationContext
+        .getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+
+    private var networkCallback : NetworkCallback? = null
+    private var scanerListener : OnScanerListener? = null
+    private var isNetworkChanged = false
+    private var isScan = false
+
+    private val wifiScanReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context : Context, intent : Intent) {
+            scanerListener?.run {
+                if (intent.getBooleanExtra(WifiManager.EXTRA_RESULTS_UPDATED, false))
+                    onScanSuccess(wifiManager)
+                else
+                    onScanFailed(wifiManager)
+
+                isScan = false
+                onScanEnd()
+            }
+
+            context.unregisterReceiver(this)
+        }
+    }
+
+    private val networkStateReceiver = object : BroadcastReceiver() {
+        @Suppress("DEPRECATION")
+        override fun onReceive(context : Context, intent : Intent) {
+            val detailedState = WifiInfo
+                .getDetailedStateOf(wifiManager.connectionInfo.supplicantState)
+
+            if (isNetworkChanged && WifiManager.NETWORK_STATE_CHANGED_ACTION.equals(intent.action)) {
+                val isMatchIP = isMatchIP(wifiManager.dhcpInfo.gateway)
+
+                if (isMatchIP && detailedState == NetworkInfo.DetailedState.OBTAINING_IPADDR) {
+                    isNetworkChanged = false
+                    networkCallback = object : ConnectivityManager.NetworkCallback() {
+                        override fun onAvailable(network : Network) {
+                            onNetworkAvailable(network)
+                            unregisterNetworkCallback()
+                        }
+
+                        override fun onUnavailable() {
+                            onNetworkUnavailable()
+                            unregisterNetworkCallback()
+                        }
+                    }
+
+                    val networkRequest = NetworkRequest.Builder()
+                        .addTransportType(NetworkCapabilities.TRANSPORT_WIFI)
+                        .removeCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+                        .build()
+
+                    connectivityManager.requestNetwork(networkRequest, networkCallback as NetworkCallback)
+                    context.unregisterReceiver(this)
+                } else if (detailedState == NetworkInfo.DetailedState.FAILED) {
+                    isNetworkChanged = false
+                    onNetworkUnavailable()
+                    context.unregisterReceiver(this)
+                }
+            }
+        }
+    }
+
+    companion object {
+        const val IP_ADDRESS = "192.168.31.15"
+        const val SCHEME_SERVER = "http://"
+        const val PORT_SERVER = "80"
+        const val PATH_WIFI_SERVER = "/wifi"
+
+        fun isMatchIP(ip : String) : Boolean {
+            return IP_ADDRESS == ip
+        }
+
+        fun isMatchIP(ip : Int) : Boolean {
+            return isMatchIP(Util.formatIPAddress(ip))
+        }
+    }
+
+    interface OnScanerListener {
+        fun onScanBegin() {}
+        fun onScanEnd() {}
+        fun onScanSuccess(wifiManager : WifiManager) {}
+        fun onScanFailed(wifiManager : WifiManager) {}
+    }
+
+    fun addWifiToModule(item : EspItem) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q)
+            connectNetworkAndroidQ(item)
+        else
+            connectNetworkPreAndroidQ(item)
+    }
+
+    @Suppress("DEPRECATION")
+    private fun connectNetworkPreAndroidQ(item : EspItem) {
+        val wifi = WifiConfiguration().apply {
+            this.SSID = "\"${item.getSsid()}\""
+            this.preSharedKey = "\"${item.getSc()}\""
+            this.status = WifiConfiguration.Status.ENABLED
+
+            this.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.TKIP)
+            this.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.CCMP)
+            this.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.WPA_PSK)
+            this.allowedPairwiseCiphers.set(WifiConfiguration.PairwiseCipher.TKIP)
+            this.allowedPairwiseCiphers.set(WifiConfiguration.PairwiseCipher.CCMP)
+            this.allowedProtocols.set(WifiConfiguration.Protocol.RSN)
+        }
+
+        wifiManager.run {
+            val id = this.addNetwork(wifi)
+
+            if (id != -1) {
+                disconnect()
+                enableNetwork(id, true)
+                reconnect()
+
+                isNetworkChanged = true
+                context.registerReceiver(networkStateReceiver, IntentFilter().apply {
+                    addAction(WifiManager.NETWORK_STATE_CHANGED_ACTION)
+                })
+            } else {
+                onNetworkUnavailable()
+            }
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.Q)
+    private fun connectNetworkAndroidQ(item : EspItem) {
+        val specifier = WifiNetworkSpecifier.Builder()
+            .setSsid(item.getSsid())
+            .setWpa2Passphrase(item.getSc())
+            .build()
+
+        val request = NetworkRequest.Builder()
+            .addTransportType(NetworkCapabilities.TRANSPORT_WIFI)
+            .removeCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+            .setNetworkSpecifier(specifier)
+            .build()
+
+        networkCallback = object : ConnectivityManager.NetworkCallback() {
+            override fun onAvailable(network : Network) {
+                onNetworkAvailable(network)
+                unregisterNetworkCallback()
+            }
+
+            override fun onUnavailable() {
+                onNetworkUnavailable()
+                unregisterNetworkCallback()
+            }
+        }
+
+        networkCallback?.let {
+            connectivityManager.requestNetwork(request, it)
+        }
+    }
+
+    private fun onNetworkAvailable(network : Network) {
+        connectivityManager.bindProcessToNetwork(network)
+        wifiManager.run {
+            val ipAddress = Util.formatIPAddress(dhcpInfo.gateway)
+
+            println("onNetworkAvailable: $network")
+            println("sendWifiToModule: $ipAddress")
+
+            if (!isMatchIP(ipAddress))
+                return onNetworkUnavailable()
+
+            println("Send wifi to module: $ipAddress")
+
+            val url = URL("${SCHEME_SERVER}${IP_ADDRESS}:${PORT_SERVER}${PATH_WIFI_SERVER}")
+            val builder = StringBuilder()
+
+            println("Connection to: ${url.toURI()}")
+
+            try {
+                with(url.openConnection() as HttpURLConnection) {
+                    requestMethod = "GET"
+
+                    inputStream.bufferedReader(Charsets.UTF_8).use {
+                        it.forEachLine { builder.append(it) }
+
+                        println("Builder: ${builder.toString()}")
+                    }
+                }
+            } catch (e : ConnectException) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    private fun onNetworkUnavailable() {
+        println("onNetworkUnavailable")
+    }
+
+    private fun unregisterNetworkCallback() {
+        connectivityManager.unregisterNetworkCallback(networkCallback!!)
+    }
+
+    fun startScanModule() {
+        if (isScan)
+            return
+
+        scanerListener?.onScanBegin()
+        context.registerReceiver(wifiScanReceiver, IntentFilter().apply {
+            addAction(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION)
+        })
+
+        isScan = true
+
+        @Suppress("DEPRECATION")
+        if (!wifiManager.startScan()) {
+            isScan = false
+
+            scanerListener?.onScanFailed(wifiManager)
+            scanerListener?.onScanEnd()
+        }
+    }
+
+    fun toggleScanModule() {
+        if (!isScan)
+            startScanModule()
+    }
+
+    fun setOnScannerListener(listener : OnScanerListener) {
+        scanerListener = listener
+    }
+
+    fun destroy() {
+        connectivityManager.unregisterNetworkCallback(networkCallback)
+    }
+
+    fun isScanRunning() : Boolean {
+        return isScan
+    }
+
+    fun isScanThrottleEnabled() : Boolean {
+        return Settings.Global.getInt(context.contentResolver, "wifi_scan_throttle_enabled") == 1
+    }
+
+}
